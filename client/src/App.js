566880ed4323705337d77082c4a167203e65065f
Alok <alokkumarswn01@gmail.com>
2025-07-20 14:25:12 +0530

    Re-added client folder as a normal directory

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+     <div className="post">
+       <div className="image">
+        <img src="https://tse1.mm.bing.net/th/id/OIP.qFKTKXW0Hmo8oXuvmTwvOwHaEH?rs=1&pid=ImgDetMain&o=7&rm=3" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Full-House backup coming later this year</h2>
+        <p className="info">
+          <a className="author">Dawid Paszko </a>
+          <time> 2023-01-06 16:45</time>
+        </p>
+        <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a ‘Whole-home Backup Power Solution.’ </p>
+        </div>
+      </div>
+     <div className="post">
+       <div className="image">
+        <img src="https://tse1.mm.bing.net/th/id/OIP.qFKTKXW0Hmo8oXuvmTwvOwHaEH?rs=1&pid=ImgDetMain&o=7&rm=3" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Full-House backup coming later this year</h2>
+        <p className="info">
+          <a className="author">Dawid Paszko </a>
+          <time> 2023-01-06 16:45</time>
+        </p>
+        <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a ‘Whole-home Backup Power Solution.’ </p>
+        </div>
+      </div>
+       <div className="post">
+       <div className="image">
+        <img src="https://tse1.mm.bing.net/th/id/OIP.qFKTKXW0Hmo8oXuvmTwvOwHaEH?rs=1&pid=ImgDetMain&o=7&rm=3" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Full-House backup coming later this year</h2>
+        <p className="info">
+          <a className="author">Dawid Paszko </a>
+          <time> 2023-01-06 16:45</time>
+        </p>
+        <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a ‘Whole-home Backup Power Solution.’ </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
